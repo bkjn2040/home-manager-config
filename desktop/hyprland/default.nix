9c9a331910aab7122fdffc8d2a38b8c797c19bc6
bkjn2040 <bkjn415@gmail.com>
2026-08-12 19:59:47 +0200

    fix: releasing submap bind exits submap

--- desktop/hyprland/default.nix
+++ desktop/hyprland/default.nix
@@ -1,85 +1,139 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
+let
+  lua = lib.generators.mkLuaInline;
+  call = args: { _args = args; };
+  dispatcher = expression: lua expression;
+  bind = keys: expression: call [ keys (dispatcher expression) ];
+  flaggedBind = keys: expression: flags: call [ keys (dispatcher expression) flags ];
+in
 {
   wayland.windowManager.hyprland = {
     enable = true;
     package = null;
     portalPackage = null;
-    extraConfig = ''
-      bind = $mod, W, submap, workspace
-      bind = $mod SHIFT, W, submap, editworkspace
-      bind = $mod, S, submap, resize
-      submap = workspace
-        bindrti = , W, submap, reset
-        bindi = , H, workspace, r-1
-        bindi = , L, workspace, r+1
-      submap = editworkspace
-        bindrti = , W, submap, reset
-        bindi = , H, movetoworkspace, r-1
-        bindi = , L, movetoworkspace, r+1
-      submap = resize
-        bindrti = , S, submap, reset
-        bindei = , H, resizeactive, -20 0
-        bindei = , L, resizeactive, 20 0
-        bindei = , K, resizeactive, 0 -20
-        bindei = , J, resizeactive, 0 20
-      submap = reset
-    '';
+
+    # Hyprland 0.55 deprecated hyprlang; 0.56 uses hyprland.lua natively.
+    configType = "lua";
+
     settings = {
-      exec-once = [
-        "hyprctl setcursor Bibata-Modern-Ice 16"
-      ];
-      "$mod" = "SUPER";
-      bind = [
-        "$mod, SPACE, exec, nc -U /run/user/1000/walker/walker.sock"
-	"$mod, Q, exit"
-	"$mod, RETURN, exec, kitty"
-	"$mod, C, killactive"
+      config = {
+        general = {
+          gaps_in = 4;
+          gaps_out = {
+            top = 0;
+            right = 16;
+            bottom = 16;
+            left = 16;
+          };
+        };
 
-	"$mod, H, movefocus, l"
-	"$mod, J, movefocus, d"
-	"$mod, K, movefocus, u"
-	"$mod, L, movefocus, r"
+        input = {
+          natural_scroll = false;
+          sensitivity = -0.1;
+          accel_profile = "flat";
+          kb_layout = "us,kr";
+          kb_options = "grp:alt_shift_toggle";
+        };
 
-	"$mod SHIFT, H, movewindow, l"
-	"$mod SHIFT, J, movewindow, d"
-	"$mod SHIFT, K, movewindow, u"
-	"$mod SHIFT, L, movewindow, r"
+        decoration.rounding = 8;
 
-        "$mod, 1, workspace, r~1"
-        "$mod, 2, workspace, r~2"
-        "$mod, 3, workspace, r~3"
-        "$mod, 4, workspace, r~4"
-        "$mod, 5, workspace, r~5"
+        misc = {
+          disable_splash_rendering = true;
+          disable_hyprland_logo = true;
+          background_color = "#b3ebf2";
+        };
+      };
 
-        "$mod SHIFT, 1, movetoworkspace, r~1"
-        "$mod SHIFT, 2, movetoworkspace, r~2"
-        "$mod SHIFT, 3, movetoworkspace, r~3"
-        "$mod SHIFT, 4, movetoworkspace, r~4"
-        "$mod SHIFT, 5, movetoworkspace, r~5"
-      ];
-      general = {
-        gaps_in  = 4;
-	gaps_out = "0,16,16,16";
-      };
-      input = {
-        natural_scroll = false;
-        sensitivity = -0.1;
-        accel_profile = "flat";
-        kb_layout = "us,kr";
-        kb_options = "grp:alt_shift_toggle";
-      };
-      decoration = {
-        rounding = 8;
-      };
-      misc = {
-        disable_splash_rendering = true;
-	disable_hyprland_logo = true;
-        background_color = "0xB3EBF2";
-      };
       monitor = [
-        "DP-2,1920x1080@144,0x0,1"
-        "HDMI-A-1,1920x1080@60,-1920x0,1"
+        {
+          output = "DP-2";
+          mode = "1920x1080@144";
+          position = "0x0";
+          scale = 1;
+        }
+        {
+          output = "HDMI-A-1";
+          mode = "1920x1080@60";
+          position = "-1920x0";
+          scale = 1;
+        }
+      ];
+
+      bind = [
+        (bind "SUPER + SPACE" ''hl.dsp.exec_cmd("nc -U /run/user/1000/walker/walker.sock")'')
+        (bind "SUPER + Q" ''hl.dsp.exec_cmd("hyprshutdown")'')
+        (bind "SUPER + RETURN" ''hl.dsp.exec_cmd("kitty")'')
+        (bind "SUPER + C" "hl.dsp.window.close()")
+
+        (bind "SUPER + H" ''hl.dsp.focus({ direction = "l" })'')
+        (bind "SUPER + J" ''hl.dsp.focus({ direction = "d" })'')
+        (bind "SUPER + K" ''hl.dsp.focus({ direction = "u" })'')
+        (bind "SUPER + L" ''hl.dsp.focus({ direction = "r" })'')
+
+        (bind "SUPER + SHIFT + H" ''hl.dsp.window.move({ direction = "l" })'')
+        (bind "SUPER + SHIFT + J" ''hl.dsp.window.move({ direction = "d" })'')
+        (bind "SUPER + SHIFT + K" ''hl.dsp.window.move({ direction = "u" })'')
+        (bind "SUPER + SHIFT + L" ''hl.dsp.window.move({ direction = "r" })'')
+
+        (bind "SUPER + 1" ''hl.dsp.focus({ workspace = "r~1" })'')
+        (bind "SUPER + 2" ''hl.dsp.focus({ workspace = "r~2" })'')
+        (bind "SUPER + 3" ''hl.dsp.focus({ workspace = "r~3" })'')
+        (bind "SUPER + 4" ''hl.dsp.focus({ workspace = "r~4" })'')
+        (bind "SUPER + 5" ''hl.dsp.focus({ workspace = "r~5" })'')
+
+        (bind "SUPER + SHIFT + 1" ''hl.dsp.window.move({ workspace = "r~1", follow = true })'')
+        (bind "SUPER + SHIFT + 2" ''hl.dsp.window.move({ workspace = "r~2", follow = true })'')
+        (bind "SUPER + SHIFT + 3" ''hl.dsp.window.move({ workspace = "r~3", follow = true })'')
+        (bind "SUPER + SHIFT + 4" ''hl.dsp.window.move({ workspace = "r~4", follow = true })'')
+        (bind "SUPER + SHIFT + 5" ''hl.dsp.window.move({ workspace = "r~5", follow = true })'')
+
+        (bind "SUPER + W" ''hl.dsp.submap("workspace")'')
+        (bind "SUPER + SHIFT + W" ''hl.dsp.submap("editworkspace")'')
+        (flaggedBind "W" ''hl.dsp.submap("reset")'' {
+          release = true;
+          transparent = true;
+          ignore_mods = true;
+          non_consuming = true;
+          submap_universal = true;
+        })
+        (bind "SUPER + S" ''hl.dsp.submap("resize")'')
+      ];
+    };
+
+    submaps = {
+      workspace.settings.bind = [
+        (flaggedBind "H" ''hl.dsp.focus({ workspace = "r-1" })'' { ignore_mods = true; })
+        (flaggedBind "L" ''hl.dsp.focus({ workspace = "r+1" })'' { ignore_mods = true; })
+      ];
+
+      editworkspace.settings.bind = [
+        (flaggedBind "H" ''hl.dsp.window.move({ workspace = "r-1", follow = true })'' { ignore_mods = true; })
+        (flaggedBind "L" ''hl.dsp.window.move({ workspace = "r+1", follow = true })'' { ignore_mods = true; })
+      ];
+
+      resize.settings.bind = [
+        (flaggedBind "S" ''hl.dsp.submap("reset")'' {
+          release = true;
+          transparent = true;
+          ignore_mods = true;
+        })
+        (flaggedBind "H" "hl.dsp.window.resize({ x = -20, y = 0, relative = true })" {
+          repeating = true;
+          ignore_mods = true;
+        })
+        (flaggedBind "L" "hl.dsp.window.resize({ x = 20, y = 0, relative = true })" {
+          repeating = true;
+          ignore_mods = true;
+        })
+        (flaggedBind "K" "hl.dsp.window.resize({ x = 0, y = -20, relative = true })" {
+          repeating = true;
+          ignore_mods = true;
+        })
+        (flaggedBind "J" "hl.dsp.window.resize({ x = 0, y = 20, relative = true })" {
+          repeating = true;
+          ignore_mods = true;
+        })
       ];
     };
   };
@@ -87,22 +141,23 @@
   home.pointerCursor =
     let
       getFrom = url: hash: name: {
+        enable = true;
         gtk.enable = true;
         x11.enable = true;
-        name = name;
+        inherit name;
         size = 16;
-        package =
-          pkgs.runCommand "moveUp" {} ''
-            mkdir -p $out/share/icons
-            ln -s ${pkgs.fetchzip {
-              url = url;
-              hash = hash;
-            }} $out/share/icons/${name}
-          '';
+        package = pkgs.runCommand "moveUp" { } ''
+          mkdir -p $out/share/icons
+          ln -s ${
+            pkgs.fetchzip {
+              inherit url hash;
+            }
+          } $out/share/icons/${name}
+        '';
       };
     in
-      getFrom
-        "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz"
-        "sha256-SG/NQd3K9DHNr9o4m49LJH+UC/a1eROUjrAQDSn3TAU="
-        "Bibata-Modern-Ice";
+    getFrom
+      "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz"
+      "sha256-SG/NQd3K9DHNr9o4m49LJH+UC/a1eROUjrAQDSn3TAU="
+      "Bibata-Modern-Ice";
 }
--- home.nix
+++ home.nix
@@ -20,14 +20,14 @@
   home.packages = [
     inputs.nixCats.packages."x86_64-linux".nixCats
 
-    (pkgs.python311.withPackages (ppkgs: [
-      ppkgs.tqdm
-      ppkgs.bqplot
-      ppkgs.numpy
-      ppkgs.scipy
-      ppkgs.matplotlib
-      ppkgs.jupyterlab
-    ]))
+    # (pkgs.python311.withPackages (ppkgs: [
+    #   ppkgs.tqdm
+    #   ppkgs.bqplot
+    #   ppkgs.numpy
+    #   ppkgs.scipy
+    #   ppkgs.matplotlib
+    #   ppkgs.jupyterlab
+    # ]))
     pkgs.scala
     pkgs.sbt
     pkgs.gcc
@@ -43,11 +43,25 @@
     pkgs.wl-clipboard
 
     pkgs.feh
+
+    pkgs.zip
+
+    pkgs.nettools
+
+    pkgs.deskreen
+
+    pkgs.davinci-resolve
+
+    pkgs.shotcut
+
+    pkgs.blender
+
+    pkgs.codex
   ];
 
   imports = [
     ./desktop/hyprland
-    ./desktop/hyprpanel
+    # ./desktop/hyprpanel
     ./desktop/walker
     ./desktop/starship
     ./desktop/tmux
@@ -62,6 +76,66 @@
     ./software/unzip
     ./software/superproductivity
   ];
+
+  services.wayle = {
+    enable = true;
+
+    # Whether to automatically install soft dependencies used by wayle that
+    # will be required based on your config.
+    autoInstallDependencies = true;
+
+    # tip: you can automatically translate your TOML config to Nix by running
+    # nix-instantiate --eval --expr 'builtins.fromTOML (builtins.readFile ./config.toml)' | nixfmt
+    settings = {
+      bar = {
+        layout = [
+          # add more attribute sets with different monitors if wayle should
+          # have different layouts on each
+          {
+            monitor = "*"; # replace "DP-1" with "*" for all monitors
+            show = true;
+            center = [
+              "clock"
+              "weather"
+            ];
+            left = [ "dashboard" ];
+            right = [ "volume" ];
+          } # this is a 'list' of 'attribute sets', no semi-colons after the closing braces needed
+        ];
+      };
+      modules = {
+        clock = {
+          format = "%H:%M:%S";
+          dropdown-show-seconds = false;
+        };
+        weather = {
+          location = "Denver";
+          units = "imperial";
+        };
+      };
+      osd = {
+        monitor = "DP-1";
+      };
+      styling = {
+        palette = {
+          bg = "#282a36";
+          blue = "#8be9fd";
+          # ...
+        };
+        # wallust will be automatically installed if this is set
+        theme-provider = "wallust";
+      };
+      # the following wallpaper option can be omitted if you're not using
+      # wayle's wallpaper engine
+      wallpaper = {
+        # this will automatically install aww
+        engine-enabled = true;
+
+        cycling-directory = "/home/horsey/Pictures/Backgrounds/1/";
+        cycling-mode = "shuffle";
+      };
+    };
+  };
 
   # Home Manager is pretty goo at managing otfiles. The primary way to manage
   # plain files is through 'home.file'.
