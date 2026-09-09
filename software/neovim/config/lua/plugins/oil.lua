fadb230e2804451a0b327b463f9f7845bfccb2c5
bkjn2040 <bkjn415@gmail.com>
2026-09-09 10:16:45 +0200

    feat: toggle Oil file explorer

--- software/neovim/config/lua/plugins/oil.lua
+++ software/neovim/config/lua/plugins/oil.lua
@@ -16,7 +16,15 @@ oil.setup({
 
 vim.keymap.set("n", "-", oil.open, { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>e", function()
+  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.bo[buf].filetype == "oil" then
+      vim.api.nvim_win_close(win, false)
+      return
+    end
+  end
+
   vim.cmd("topleft 32vsplit")
   oil.open(vim.fn.getcwd())
   vim.wo.winfixwidth = true
-end, { desc = "Open directory sidebar" })
+end, { desc = "Toggle directory sidebar" })
